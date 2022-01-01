3d3be3f3d45b1a4bb1ea66d6055e25249d3e366b
Doublec98 <67845379+Doublec98@users.noreply.github.com>
2022-01-01 19:55:58 +0100

    Update pause.js

--- scripts/pause.js
+++ scripts/pause.js
@@ -1,1 +1,12 @@
 
+var video = document.getElementById("video-1");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
